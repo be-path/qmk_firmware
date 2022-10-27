7b8941d5ef14eac2851709948a49c04ee5f615d3
Narumichi Sakai <sakai@enta-bridge.com>
2022-10-27 15:42:26 +0900

    fix miniaxe:nal

--- keyboards/miniaxe/keymaps/nal/keymap.c
+++ keyboards/miniaxe/keymaps/nal/keymap.c
@@ -32,28 +32,28 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_3x5_3( \
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,   KC_I,    KC_O,    KC_P, \
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,   KC_K,    KC_L,    LCTL_T(KC_SCLN), \
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,   KC_COMM, KC_DOT,  LSFT_T(KC_SLSH), \
-                      KC_LALT, KC_LGUI, KC_LSFT,      KC_MINS, KC_SPC, KC_TAB \
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,   KC_I,    KC_O,   KC_P, \
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, \
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,   KC_COMM, KC_DOT, LSFT_T(KC_SLSH), \
+                      KC_LALT, KC_LGUI, KC_LSFT,      KC_LCTL, KC_SPC, KC_TAB \
   ),
   [_LOWER] = LAYOUT_split_3x5_3( \
-    IMEOFF, IMEON, KC_LBRC, KC_RBRC, KC_QUOT,      KC_ESC,  KC_HOME, KC_UP,   KC_DEL,   KC_ENT, \
-    KC_LT,  KC_GT, KC_LCBR, KC_RCBR, KC_DQUO,      KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, LCTL_T(KC_END), \
-    KC_NO,  KC_NO, KC_LPRN, KC_RPRN, KC_NO,        KC_BSPC,  KC_NO,   KC_NO,   KC_DEL,   _______, \
-                  _______, _______, _______,      _______,  _______, _______ \
+    IMEOFF,  IMEON,   KC_LBRC, KC_RBRC, KC_QUOT,     KC_ESC,  KC_HOME, KC_UP,   KC_DEL,   KC_ENT, \
+    KC_MINS, KC_UNDS, KC_LCBR, KC_RCBR, KC_DQUO,     KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, \
+    KC_LT,   KC_GT,   KC_LPRN, KC_RPRN, KC_NO,       KC_BSPC, KC_NO,   KC_NO,   KC_DEL,   _______, \
+    _______, _______, _______,                       _______,  _______, _______ \
   ),
   [_RAISE] = LAYOUT_split_3x5_3( \
-    KC_NO, KC_7, KC_8, KC_9, KC_NO,      KC_GRV, KC_AMPR, KC_ASTR, KC_BSLS, KC_NO, \
+    KC_NO, KC_7, KC_8, KC_9, KC_NO,      KC_NO,  KC_AMPR, KC_ASTR, KC_BSLS, KC_NO, \
     KC_NO, KC_4, KC_5, KC_6, KC_NO,      KC_EQL, KC_DLR,  KC_PERC, KC_CIRC, KC_NO, \
-    KC_0,  KC_1, KC_2, KC_3, KC_NO,      KC_NO,  KC_EXLM, KC_AT,   KC_HASH, KC_NO, \
-                      _______, _______, _______,      _______, _______, _______ \
+    KC_0,  KC_1, KC_2, KC_3, KC_NO,      KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_NO, \
+    _______, _______, _______,           _______, _______, _______ \
   ),
   [_ADJUST] =  LAYOUT_split_3x5_3( \
     RESET,  KC_F7, KC_F8, KC_F9, KC_NO,      KC_NO,  KC_NO, KC_NO,   KC_NO, KC_NO, \
     KC_NO,  KC_F4, KC_F5, KC_F6, KC_NO,      KC_NO,  KC_NO, KC_NO,   KC_NO, KC_NO, \
     KC_NO,  KC_F1, KC_F2, KC_F3, KC_NO,      KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, \
-                      _______, _______, _______,      _______, _______, _______ \
+    _______, _______, _______,               _______, _______, _______ \
   )
 };
 
