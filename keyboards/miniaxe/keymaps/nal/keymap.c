08118eb63c2c2f1c70443afba1debefc3341632a
Narumichi Sakai <sakai@enta-bridge.com>
2022-12-30 07:46:19 +0900

    fix miniaxe:nal

--- keyboards/miniaxe/keymaps/nal/keymap.c
+++ keyboards/miniaxe/keymaps/nal/keymap.c
@@ -31,25 +31,33 @@ enum custom_keycodes {
   ADJUST,
   IMEON,
   IMEOFF,
+  THUMB_L1,
+  THUMB_L2,
+  THUMB_L2W,
+  THUMB_L3,
+  THUMB_R1,
+  THUMB_R1W,
+  THUMB_R2,
+  THUMB_R3,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_3x5_3( \
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,   KC_I,    KC_O,   KC_P, \
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, \
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,   KC_COMM, KC_DOT, LSFT_T(KC_SLSH), \
-                      KC_LALT, KC_LGUI, KC_LSFT,      KC_LCTL, KC_SPC, KC_TAB \
+    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, \
+    THUMB_L1, THUMB_L2, LSFT_T(KC_TAB),                     THUMB_R1, THUMB_R2, THUMB_R3 \
   ),
   [_WIN] =  LAYOUT_split_3x5_3( \
     _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, \
-    _______, KC_LCTL, _______,                        KC_LGUI, _______, _______ \
+    _______, THUMB_L2W, _______,                      THUMB_R1W, _______, _______ \
   ),
   [_LOWER] = LAYOUT_split_3x5_3( \
-    IMEOFF,  IMEON,   KC_LBRC, KC_RBRC, KC_QUOT,     KC_ESC,  KC_HOME, KC_UP,   KC_DEL,   KC_ENT, \
+    KC_TAB,  KC_NO,   KC_LBRC, KC_RBRC, KC_QUOT,     KC_ESC,  KC_HOME, KC_UP,   KC_DEL,   KC_ENT, \
     KC_MINS, KC_UNDS, KC_LCBR, KC_RCBR, KC_DQUO,     KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END, \
-    KC_LT,   KC_GT,   KC_LPRN, KC_RPRN, KC_EQL,      KC_BSPC, KC_NO,   KC_NO,   KC_DEL,   _______, \
+    KC_LT,   KC_GT,   KC_LPRN, KC_RPRN, KC_EQL,      KC_BSPC, KC_NO,   KC_NO,   KC_DEL,   KC_NO, \
     _______, _______, _______,                       _______,  _______, _______ \
   ),
   [_RAISE] = LAYOUT_split_3x5_3( \
@@ -58,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_0,  KC_1, KC_2, KC_3, KC_NO,      KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_NO, \
     _______, _______, _______,           _______, _______, _______ \
   ),
-  [_ADJUST] =  LAYOUT_split_3x5_3( \
+  [_ADJUST] = LAYOUT_split_3x5_3( \
     RESET,  KC_F7, KC_F8, KC_F9, KC_NO,      KC_NO,  KC_NO, KC_NO,   KC_NO, TG(_WIN), \
     KC_NO,  KC_F4, KC_F5, KC_F6, KC_NO,      KC_NO,  KC_NO, KC_NO,   KC_NO, KC_NO, \
     KC_NO,  KC_F1, KC_F2, KC_F3, KC_NO,      KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, \
@@ -79,6 +87,85 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         // persistant_default_layer_set(1UL<<_QWERTY);
         set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case THUMB_L1:
+      if (record->event.pressed) {
+        register_code(KC_LALT);
+      } else {
+        unregister_code(KC_LALT);
+        if (is_tapped) {
+          tap_code(KC_HENK);
+          tap_code(KC_LANG1);
+        }
+      }
+      break;
+    case THUMB_L2:
+      if (record->event.pressed) {
+        register_code(KC_LGUI);
+      } else {
+        unregister_code(KC_LGUI);
+      }
+      break;
+    case THUMB_L2W:
+      if (record->event.pressed) {
+        register_code(KC_LCTL);
+      } else {
+        unregister_code(KC_LCTL);
+      }
+      break;
+    case THUMB_L3:
+      if (record->event.pressed) {
+        register_code(KC_LSFT);
+      } else {
+        unregister_code(KC_LSFT);
+        if (is_tapped) {
+          tap_code(KC_TAB);
+        }
+      }
+      break;
+    case THUMB_R1:
+      if (record->event.pressed) {
+        register_code(KC_LCTL);
+      } else {
+        unregister_code(KC_LCTL);
+        if (is_tapped) {
+          tap_code(KC_SPC);
+        }
+      }
+      break;
+    case THUMB_R1W:
+      if (record->event.pressed) {
+        register_code(KC_LGUI);
+      } else {
+        unregister_code(KC_LGUI);
+        if (is_tapped) {
+          tap_code(KC_SPC);
+        }
+      }
+      break;
+    case THUMB_R2:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case THUMB_R3:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        if (is_tapped) {
+          tap_code(KC_MHEN);
+          tap_code(KC_LANG2);
+        }
       }
       return false;
       break;
@@ -117,32 +204,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       } else {
         layer_off(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case KC_SPC:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-        if (is_tapped) {
-          tap_code(keycode);
-        }
-      }
-      return false;
-      break;
-    case KC_TAB:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-        if (is_tapped) {
-          tap_code(keycode);
-        }
       }
       return false;
       break;
